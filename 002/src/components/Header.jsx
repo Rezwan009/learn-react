106182eeb7f4d2eac47fd3cfa359c85908835c83
Rezwan Ahmed <arezwan93@gmail.com>
2026-09-09 16:32:42 +0600

    Add initial CSS styles for layout and design
    - Create a new index.css file to define global styles and layout.
    - Set box-sizing to border-box for consistent sizing.
    - Style the body with a default font and color.
    - Design header with flexbox for logo and navigation.
    - Add hover effects for logo and navigation items.
    - Style main content area with background image and padding.
    - Define footer styles for fixed positioning and appearance.

--- 002/src/components/Header.jsx
+++ 002/src/components/Header.jsx
@@ -1,10 +1,10 @@
 export default function Header() {
   return (
-    <header className="header">
-      <div className="logo">
-        <img src="./src/assets/react.svg" alt="React logo" />
-      </div>
-      <nav>
+    <header>
+      <nav className="header">
+        <div className="logo">
+          <img src="./src/assets/react.svg" alt="React logo" />
+        </div>
         <ul className="nav-list">
           <li className="nav-list-item">Pricing</li>
           <li className="nav-list-item">About</li>
--- 002/src/components/MainContent.jsx
+++ 002/src/components/MainContent.jsx
@@ -1,8 +1,8 @@
 export default function MainContent() {
   return (
-    <main>
+    <main className="main-content">
       <h1>Reason I am excited to learn React</h1>
-      <ol>
+      <ul className="main-content-list">
         <li>
           React is a popular library, so I will be able to fit in with all the
           coolest devs out there! 😎
@@ -10,7 +10,16 @@ export default function MainContent() {
         <li>
           I am more likely to get a job as a front end developer if I know React
         </li>
-      </ol>
+        <li>
+          I am more likely to get a job as a front end developer if I know React
+        </li>
+        <li>
+          I am more likely to get a job as a front end developer if I know React
+        </li>{" "}
+        <li>
+          I am more likely to get a job as a front end developer if I know React
+        </li>
+      </ul>
     </main>
   );
 }
