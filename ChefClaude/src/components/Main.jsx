dc9502b012009ababd26df26f4961f4d17765da9
Rezwan Ahmed <arezwan93@gmail.com>
2026-09-13 16:05:09 +0600

    feat: implement ingredient addition functionality in Main component

--- ChefClaude/src/components/Main.jsx
+++ ChefClaude/src/components/Main.jsx
@@ -1,14 +1,36 @@
+import { useState } from "react";
+
 const Main = () => {
+  const [ingredients, setIngredients] = useState([
+    "Chicken",
+    "Oregano",
+    "Tomatoes",
+  ]);
+  const ingredientsListItems = ingredients.map((ingredient) => (
+    <li key={ingredient}>{ingredient}</li>
+  ));
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const newIngredient = formData.get("ingredient");
+    setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
+    event.currentTarget.reset();
+  }
+
   return (
     <main>
-      <form className="add-ingredient-form">
+      <form onSubmit={handleSubmit} className="add-ingredient-form">
         <input
           type="text"
           placeholder="e.g. oregano"
           aria-label="Add Ingredient"
+          name="ingredient"
         />
         <button>Add Ingredient</button>
       </form>
+
+      <ul>{ingredientsListItems}</ul>
     </main>
   );
 };
