f0fd4c04d05f6adf9f7afa7db3bd71c221109fb0
Rezwan Ahmed <arezwan93@gmail.com>
2026-09-16 17:00:31 +0600

    feat: enhance game functionality with win detection and accessibility improvements

--- RollDice/src/App.jsx
+++ RollDice/src/App.jsx
@@ -1,9 +1,19 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Dice from "./components/Dice";
 import { nanoid } from "nanoid";
 import Confetti from "react-confetti";
 function App() {
   const [dice, setDice] = useState(() => generateAllNewDice());
+  const buttonRef = useRef(null);
+  const gameWon =
+    dice.every((die) => die.isHeld) &&
+    dice.every((die) => die.value === dice[0].value);
+
+  useEffect(() => {
+    if (gameWon) {
+      buttonRef.current.focus();
+    }
+  }, [gameWon]);
 
   function generateAllNewDice() {
     // Option 1
@@ -21,10 +31,6 @@ function App() {
       isHeld: false,
     }));
   }
-
-  const gameWon =
-    dice.every((die) => die.isHeld) &&
-    dice.every((die) => die.value === dice[0].value);
 
   function hold(id) {
     setDice((oldDice) =>
@@ -59,17 +65,23 @@ function App() {
   const diceElements = dice.map((item) => (
     <Dice key={item.id} item={item} hold={hold} />
   ));
+
   return (
     <>
       <main>
         {gameWon && <Confetti />}
+        <div aria-live="polite" className="sr-only">
+          {gameWon && (
+            <p>Congratulations! You won! Press "New Game" to start again.</p>
+          )}
+        </div>
         <h1 className="title">Tenzies</h1>
         <p className="instructions">
           Roll until all dice are the same. Click each die to freeze it at its
           current value between rolls.
         </p>
         <div className="dice-container">{diceElements}</div>
-        <button className="roll-dice" onClick={rollDice}>
+        <button ref={buttonRef} className="roll-dice" onClick={rollDice}>
           {gameWon ? "New Game" : "Roll"}
         </button>
       </main>
--- RollDice/src/components/Dice.jsx
+++ RollDice/src/components/Dice.jsx
@@ -6,6 +6,9 @@ const Dice = ({ item, hold }) => {
         color: item.isHeld ? "white" : "black",
       }}
       onClick={() => hold(item.id)}
+      aria-pressed={item.isHeld}
+      aria-label={`Die with value ${item.value}, 
+            ${item.isHeld ? "held" : "not held"}`}
     >
       {item.value}
     </button>
