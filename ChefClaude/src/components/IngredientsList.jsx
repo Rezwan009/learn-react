2b99abc9ff00d473bf94942d8731eb8049d57a08
Rezwan Ahmed <arezwan93@gmail.com>
2026-09-16 12:47:59 +0600

    feat: enhance IngredientsList and Main components with loading state and recipe section scrolling

--- ChefClaude/src/components/IngredientsList.jsx
+++ ChefClaude/src/components/IngredientsList.jsx
@@ -1,4 +1,9 @@
-const IngredientsList = ({ ingredients, getRecipe }) => {
+const IngredientsList = ({
+  ingredients,
+  getRecipe,
+  recipeSection,
+  loading,
+}) => {
   const ingredientsListItems = ingredients.map((ingredient) => (
     <li key={ingredient}>{ingredient}</li>
   ));
@@ -11,11 +16,19 @@ const IngredientsList = ({ ingredients, getRecipe }) => {
       </ul>
       {ingredients.length > 3 && (
         <div className="get-recipe-container">
-          <div>
+          <div ref={recipeSection}>
             <h3>Ready for a recipe?</h3>
             <p>Generate a recipe from your list of ingredients.</p>
           </div>
-          <button onClick={getRecipe}>Get a recipe</button>
+          <button onClick={getRecipe} disabled={loading}>
+            Get a recipe
+          </button>
+        </div>
+      )}
+      {loading && (
+        <div className="loader-container">
+          <div className="loader"></div>
+          <p>Cooking up a delicious recipe...</p>
         </div>
       )}
     </section>
--- ChefClaude/src/components/Main.jsx
+++ ChefClaude/src/components/Main.jsx
@@ -1,22 +1,38 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ClaudeRecipe from "./ClaudeRecipe";
 import IngredientsList from "./IngredientsList";
 import { getRecipeFromMistral } from "../ai";
 const Main = () => {
-  const [ingredients, setIngredients] = useState([
-    "all the main spices",
-    "pasta",
-    "ground beef",
-    "tomato paste",
-  ]);
-
+  const [ingredients, setIngredients] = useState([]);
+  const [loading, setLoading] = useState(false);
   const [recipe, setRecipe] = useState("");
+  const recipeSection = useRef(null);
+
+  useEffect(() => {
+    if (recipe !== "" && recipeSection.current !== null) {
+      recipeSection.current.scrollIntoView({ behavior: "smooth" });
+
+      // const yCoord =
+      //   recipeSection.current.getBoundingClientRect().top + window.scrollY;
+      // window.scroll({
+      //   top: yCoord,
+      //   behavior: "smooth",
+      // });
+    }
+  }, [recipe]);
 
   async function getRecipe() {
-    // setRecipeShown((prev) => !prev);
-    const recipeMarkdown = await getRecipeFromMistral(ingredients);
+    try {
+      setLoading(true);
 
-    setRecipe(recipeMarkdown);
+      const recipeMarkdown = await getRecipeFromMistral(ingredients);
+
+      setRecipe(recipeMarkdown);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
   }
 
   function addIngredient(formData) {
@@ -44,7 +60,12 @@ const Main = () => {
 
       {/* Ingredients List details*/}
       {ingredients.length > 0 && (
-        <IngredientsList ingredients={ingredients} getRecipe={getRecipe} />
+        <IngredientsList
+          loading={loading}
+          recipeSection={recipeSection}
+          ingredients={ingredients}
+          getRecipe={getRecipe}
+        />
       )}
       {/* Recipe details */}
       {recipe && <ClaudeRecipe recipe={recipe} />}
